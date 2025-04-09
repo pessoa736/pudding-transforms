27e3d82ddc1545622c148b09ac09ef2ace6c301e
Davi <fenandosouzamelo419@gmail.com>
2025-04-09 20:06:23 -0300

    criação de componetes para front-end

--- App/app.lua
+++ App/app.lua
@@ -5,6 +5,12 @@ local app = lapis.Application()
 app:enable("etlua")
 
 
+--app:match("/", function(self)
+--  return self:html(function()
+--    head()
+--  end)
+-- end)
+  
 app:get("/", function()
   return { render = "index"}
 end)
