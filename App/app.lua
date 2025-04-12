fce6b108ae76cb3903bfb561aeac35bfc5f554d4
Davi dos Santos Passos <fenandosouzamelo419@gmail.com>
2025-04-11 22:11:10 -0300

    update

--- App/app.lua
+++ App/app.lua
@@ -1,18 +1,20 @@
 local lapis = require("lapis")
 local config = require("lapis.config").get()
 
+local helpers = require("static.scripts.helpers")
+
 local app = lapis.Application()
 app:enable("etlua")
 
-
---app:match("/", function(self)
---  return self:html(function()
---    head()
---  end)
--- end)
+app:before_filter(function(self)
+  self.helpers = helpers
+end)
   
-app:get("/", function()
+app:get("home", "/", function()
   return { render = "index"}
+end)
+app:get("visualizador de modelo", "/visualizador-de-modelo", function()
+  return { render = "visualizador-de-modelo"}
 end)
 
 return app
---
+++ App/static/scripts/helpers.lua
@@ -0,0 +1,8 @@
+
+local helpers = {}
+
+function helpers.get_style()
+    return '<link rel="stylesheet" href="static/styles.css">'
+end
+
+return helpers
